b75d59503df2af7c7b81f903c7ae7382d64d2f92
ManeRomero <algoritmo717@gmail.com>
2019-05-13 17:55:29 +0200

    4th commit - finished functions and scss layout!

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import database from './films.js';
 
-const URL = 'http://image.tmdb.org/t/p/w185/'
+window.addEventListener('load', () => {
+    const URL = 'http://image.tmdb.org/t/p/w185/'
 let container = document.querySelector('section')
 
 for (let film of database) {
@@ -8,27 +9,37 @@ for (let film of database) {
     let date = document.createElement('small')
     let pic = document.createElement('img')
     let title = document.createElement('h1')
-    let rating = document.createElement('small')
-
+    let rateContainer = document.createElement('div')
+    let countStars = film.vote_average / 2
+    let maxVoteValue = 5
+    let divBottom = document.createElement('div')
+    divBottom.className = 'divBottom'
     card.className = 'card'
+    rateContainer.className = 'voteContain'
     date.innerText = film.release_date
     title.innerText = film.title
-    rating.innerText = film.vote_average
     pic.setAttribute('src', URL + film.poster_path)
+    rateContainer.setAttribute('title', `Valoración del público: ${film.vote_average}`)
 
+
+    for (let i = 0; i < maxVoteValue; i++) {
+        if (i < countStars) {
+            let icon = document.createElement('i')
+            icon.className = 'fas fa-star'
+            rateContainer.appendChild(icon)
+        } else {
+            let icon = document.createElement('i')
+            icon.className = 'far fa-star'
+            rateContainer.appendChild(icon)
+        }
+    }
+
+    divBottom.appendChild(title)
+    divBottom.appendChild(rateContainer)
 
     card.appendChild(date)
     card.appendChild(pic)
-    card.appendChild(title)
-    card.appendChild(rating)
-    
+    card.appendChild(divBottom)    
     container.appendChild(card)
 }
-
-
-
-
-
-
-
-console.log('AL PARECER, ESTA MOVIDA ENTRA! \n', database)
+})
