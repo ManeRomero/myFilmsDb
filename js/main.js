64b6a95563318ba2be377f9d7eb1e60aeed225a9
ManeRomero <algoritmo717@gmail.com>
2019-05-14 10:45:35 +0200

    5th commit - destructured 'let' assignment

--- js/main.js
+++ js/main.js
@@ -2,24 +2,30 @@ import database from './films.js';
 
 window.addEventListener('load', () => {
     const URL = 'http://image.tmdb.org/t/p/w185/'
-let container = document.querySelector('section')
+    let container = document.querySelector('section')
 
-for (let film of database) {
-    let card = document.createElement('div')
-    let date = document.createElement('small')
-    let pic = document.createElement('img')
-    let title = document.createElement('h1')
-    let rateContainer = document.createElement('div')
-    let countStars = film.vote_average / 2
-    let maxVoteValue = 5
-    let divBottom = document.createElement('div')
-    divBottom.className = 'divBottom'
-    card.className = 'card'
-    rateContainer.className = 'voteContain'
-    date.innerText = film.release_date
-    title.innerText = film.title
-    pic.setAttribute('src', URL + film.poster_path)
-    rateContainer.setAttribute('title', `Valoración del público: ${film.vote_average}`)
+    for (let film of database) {
+        let {release_date, title, poster_path, vote_average} = film
+
+        let countStars = vote_average / 2
+        let maxVoteValue = 5
+
+        let card = document.createElement('div')
+        let date = document.createElement('small')
+        let pic = document.createElement('img')
+        let titleCard = document.createElement('h1')
+        let rateContainer = document.createElement('div')
+        let divBottom = document.createElement('div')
+
+        divBottom.className = 'divBottom'
+        card.className = 'card'
+        rateContainer.className = 'voteContain'
+
+
+        date.innerText = release_date
+        titleCard.innerText = title
+        pic.setAttribute('src', URL + poster_path)
+        rateContainer.setAttribute('title', `Valoración del público: ${vote_average}`)
 
 
     for (let i = 0; i < maxVoteValue; i++) {
@@ -34,12 +40,12 @@ for (let film of database) {
         }
     }
 
-    divBottom.appendChild(title)
+    divBottom.appendChild(titleCard)
     divBottom.appendChild(rateContainer)
 
     card.appendChild(date)
     card.appendChild(pic)
     card.appendChild(divBottom)    
     container.appendChild(card)
-}
+    }
 })
