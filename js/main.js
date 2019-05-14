93b152e7cde15a586c37f68764e570411bb9e48c
ManeRomero <algoritmo717@gmail.com>
2019-05-14 13:11:58 +0200

    6th commit - time to test another way!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import database from './films.js';
+const URL = 'http://image.tmdb.org/t/p/w185/'
+let idFake = 0
 
 window.addEventListener('load', () => {
-    const URL = 'http://image.tmdb.org/t/p/w185/'
     let container = document.querySelector('section')
 
     for (let film of database) {
-        let {release_date, title, poster_path, vote_average} = film
-
+        let {release_date, title, poster_path, vote_average, overview} = film
         let countStars = vote_average / 2
         let maxVoteValue = 5
 
@@ -16,14 +16,21 @@ window.addEventListener('load', () => {
         let titleCard = document.createElement('h1')
         let rateContainer = document.createElement('div')
         let divBottom = document.createElement('div')
+        let descriptionText = document.createElement('small')
+        let descriptionIcon = document.createElement('i')
+
 
         divBottom.className = 'divBottom'
         card.className = 'card'
         rateContainer.className = 'voteContain'
-
+        descriptionIcon.className = 'fas fa-eye'
+        descriptionIcon.id = 'icon_' + idFake
+        descriptionText.className = 'description'
+        idFake++
 
         date.innerText = release_date
         titleCard.innerText = title
+        descriptionText.innerText = overview
         pic.setAttribute('src', URL + poster_path)
         rateContainer.setAttribute('title', `Valoración del público: ${vote_average}`)
 
@@ -45,7 +52,23 @@ window.addEventListener('load', () => {
 
     card.appendChild(date)
     card.appendChild(pic)
-    card.appendChild(divBottom)    
+    card.appendChild(divBottom)
+    card.appendChild(descriptionIcon)
+    card.appendChild(descriptionText)
     container.appendChild(card)
+
+/*  
+    tratar de hacer un toggleButton
+
+    let listener = document.querySelector('#icon_')
+    descriptionIcon.addEventListener('click', show)
+
+    function show (evt) {
+        let selected = evt.innerHtml.id
+        console.log(selected)
     }
+*/
+    }
+
+    
 })
